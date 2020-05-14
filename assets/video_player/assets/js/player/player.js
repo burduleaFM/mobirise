c8a911933e89164cc8f58a807c2dbd65c8d4c020
alex <sandel14@mail.ru>
2020-05-14 15:19:11 +0300

    added all resources

---
+++ assets/video_player/assets/js/player/player.js
@@ -0,0 +1,29 @@
+var frm = $('#bt_24');
+
+frm.submit(function (e) {
+
+    e.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: frm.attr('action'),
+        data: frm.serialize(),
+        success: function (data) {
+            console.log('Submission was successful.');
+
+            $('#success-alert').removeAttr("hidden");
+
+            $('#name').val("")
+            $('#email').val("")
+            $('#phone').val("")
+            $('#company').val("")
+            $('#post').val("")
+            $('#comments').val("")
+
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            $("#error-alert").removeAttr("hidden")
+        },
+    });
+});
